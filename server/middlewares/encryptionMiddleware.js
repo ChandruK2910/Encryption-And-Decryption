b09d731d7db7a3a384fc0492f75e8ee6eb611763
AstickDutta <astickdutta19@gmail.com>
2023-09-16 17:05:34 +0530

    push

--- server/middlewares/encryptionMiddleware.js
+++ server/middlewares/encryptionMiddleware.js
@@ -1,1 +1,41 @@
-// Function to encrypt the request body
+// Function to encrypt the request body// middleware/encryption.js
+const forge = require("node-forge");
+
+function encryptRequestBody(key, iv) {
+  return (req, res, next) => {
+    const cipher = forge.cipher.createCipher(
+      "AES-CBC",
+      forge.util.hexToBytes(key)
+    );
+    cipher.start({ iv: forge.util.hexToBytes(iv) });
+
+    const requestBody = JSON.stringify(req.body);
+    const encryptedRequestBody = cipher.update(
+      forge.util.createBuffer(requestBody, "utf8")
+    );
+    cipher.finish();
+
+    req.encryptedBody = encryptedRequestBody.toHex();
+    next();
+  };
+}
+
+function decryptResponseBody(key, iv) {
+  return (req, res, next) => {
+    const decipher = forge.cipher.createDecipher(
+      "AES-CBC",
+      forge.util.hexToBytes(key)
+    );
+    decipher.start({ iv: forge.util.hexToBytes(iv) });
+
+    const responseText = res.locals.responseText;
+    const decryptedResponseBody = decipher.update(
+      forge.util.createBuffer(forge.util.hexToBytes(responseText))
+    );
+    decipher.finish();
+
+    res.json(JSON.parse(decryptedResponseBody.toString("utf8")));
+  };
+}
+
+module.exports = { encryptRequestBody, decryptResponseBody };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/userController');
+const AuthController = require('../controllers/userController');
+const { encryptRequestBody, decryptResponseBody }= require("../middlewares/encryptionMiddleware")
 
-router.post('/register', register);
-router.post('/login', login);
+const key = process.env.KEY;
+const iv = process.env.CIPHER_IV;
+
+
+router.post('/register', encryptRequestBody(key, iv), AuthController.register);
+router.post('/login', encryptRequestBody(key, iv), AuthController.login);
+
+router.use(decryptResponseBody(key, iv));
 
 module.exports = router;
