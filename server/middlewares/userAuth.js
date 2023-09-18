f5b7fe7d51e209cd4e5ab98a0b36d50b3da73a5f
AstickDutta <astickdutta19@gmail.com>
2023-09-18 12:04:26 +0530

    push

--- server/middlewares/userAuth.js
+++ server/middlewares/userAuth.js
@@ -1,19 +1,26 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-function authenticateToken(req, res, next) {
-  const token = req.header('auth-token');
+const authenticateToken = (req, res, next) => {
+  const token = req.header("auth-token");
 
-  if (!token) return res.status(401).send('Access Denied');
+  if (!token)
+    return res.status(401).send({ status: false, message: "token missing" });
 
   try {
     const verified = jwt.verify(token, process.env.SECRET_KEY);
     req.user = verified;
     next();
   } catch (err) {
-    res.status(400).send('Invalid Token');
+    res.status(400).send({ status: false, message: "Invalid Token" });
   }
-}
+};
 
+const authorizeToken = (req, res, next) => {
+  if (req.params.userId === req.user._id.toString()) {
+    next();
+  } else {
+    res.status(403).send({ status: false, message: "Access denied." });
+  }
+};
 
-
-module.exports = { authenticateToken };
+module.exports = { authenticateToken, authorizeToken };
